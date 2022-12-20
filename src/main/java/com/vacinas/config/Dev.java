692abbd378d4ef025fcc6b0984b5afe2bde301ef
Nepsther <kenneth.nepsther@gmail.com>
2022-12-20 20:26:48 +0100

    configuration of the development profile and addition of data mass (#2)
    Co-authored-by: kenneth luzolo <kenneth.nepsther@gmai.com>

--- src/main/java/com/vacinas/config/Dev.java
+++ src/main/java/com/vacinas/config/Dev.java
@@ -1,5 +1,6 @@
 package com.vacinas.config;
 
+import com.vacinas.enums.VaccineIntakeRoute;
 import com.vacinas.model.VaccineIngestionMode;
 import com.vacinas.model.VaccineModel;
 import com.vacinas.repository.VaccineIngestionModeRepository;
@@ -8,7 +9,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Random;
 
 @Configuration
 @Profile("dev")
@@ -20,27 +23,44 @@ public class Dev {
         this.vaccineRepository = vaccineRepository;
         this.vaccineIngestionModeRepository = vaccineIngestionModeRepository;
     }
-    int i,n1,n2;
-    String vaccine[] ={"Kenneth","Lansieth","Mimosa","Nsimba","Lando","Lajy","Adilson","Vani"};
-    String description[] ={"Kenneth","Lansieth","Mimosa","Nsimba","Lando","Lajy","Adilson","Vani"};
-//@Bean
-//    public void startDB() {
-//
-//    var category1 = new VaccineIngestionMode(null,"Atenuadas");
-//    var category2 = new VaccineIngestionMode(null,"Inativadas");
-//    var category3 = new VaccineIngestionMode(null,"Subunidades");
-//        vaccineIngestionModeRepository.saveAll(List.of(category1,category2,category3));
-//
-//        for (int i = 0; i < 20; i++) {
-//            var vaccine = new VaccineModel(null,"Vaccine","description","type" );
-//            vaccineRepository.save(vaccine);
-//
-//
-//        }
+       String []vaccine ={"BCG","Hepatite B","Penta","Polio inativada","Polio oral","Rotavírus",
+            "Pneumocócica 10-valente","Meningocócica C","Febre amarela"," Tríplice viral (SCR-sarampo caxumba e,rubéola)"};
+
+    String[] age = {"Ao nascer", "2 meses", "4 meses", "6 meses", "8 meese", "10 meses", "1 ano", "2 anos", "10 anos", "14 anos"};
+    String[] allotment = {"12121-121", "2121-12", "273156-a", "74574-2", "984587-e33"};
+    VaccineIntakeRoute[] intakeRoute = {VaccineIntakeRoute.ORAL, VaccineIntakeRoute.INTRADÉRMICA, VaccineIntakeRoute.SUBCUTÂNEA};
+    String schedule = "Lorem Ipsum is simply dummy text of the printing printer took a galley";
+    String description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry." +
+            " the industry's standard dummy text ever since the 1500s, when an unknown printer tookey";
+
+@Bean
+    public void startDB() {
+
+    var random = new Random();
+
+    for (int i = 0; i < 10; i++) {
+
+
+       int n1 = 0 + random.nextInt(10);
+       int n2 = 0 + random.nextInt(5);
+        int n3 = 0 + random.nextInt(3);
+
+        var ingestionMode1 = new VaccineIngestionMode(null, age[n1], schedule, description, null);
+
+        vaccineIngestionModeRepository.save(ingestionMode1);
+
+
+
+        var vaccine1 = new VaccineModel(null, vaccine[n1], description, allotment[n2], "PALANCA",
+                LocalDateTime.now(), LocalDateTime.of(4,4,4,4,4), intakeRoute[n3], null);
+        vaccineRepository.save(vaccine1);
+
+
+    }
 
 
 
 
-  //  }
+    }
 
 }
